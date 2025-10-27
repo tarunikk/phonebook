873b7fadfd2c946364a7fd52ad492d83202ae29b
Taru <tarunikkanen@gmail.com>
2025-10-27 21:28:19 +0200

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var morgan = require('morgan')
 
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 let persons = [
   {
     id: "1",
@@ -134,6 +136,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
