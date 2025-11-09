d83146998b3da003d38b9d10795ea5dc81c2d18f
Taru <tarunikkanen@gmail.com>
2025-11-09 17:44:38 +0200

    uibuild

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ var morgan = require('morgan')
 
 const app = express()
 
-app.use(express.json())
+morgan.token('type', function (req, res) { 
+    const body = req.body
+    if (body == null) {
+        return "-"
+    } else {
+    return JSON.stringify([body.name, body.number])}
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)  
+}
 
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
 // Tehty 3.9-3.11 
 // Frontendistä tuotantoversio (dist) ja commit gittiin kutsulla "npm run deploy:full"
@@ -40,16 +58,6 @@ let persons = [
   }
 ]
 
-morgan.token('type', function (req, res) { 
-    const body = req.body
-    if (body == null) {
-        return "-"
-    } else {
-    return JSON.stringify([body.name, body.number])}
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-
 
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>')
@@ -72,27 +80,17 @@ app.get('/api/persons', (request, response) => {
 })
 
 // Yksittäisen henkilön tietojen tarkastelu
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next (error))
 })
-
-
-/*
-app.post('/api/persons', (request, response) => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id))) 
-    : 0
-
-  const person = request.body
-  person.id = String(maxId + 1)
-
-  persons = persons.concat(person)
-  console.log(person) 
-  response.json(person)
-})
-*/
 
 
 const generateId = () => {
@@ -145,13 +143,22 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
+
+// olemattomien osoitteiden käsittely
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
